31f45a3459ef92f49b07ff55059423433f526211
Maria <melnikovamaria1708@gmail.com>
2023-03-17 16:18:20 +0500

    Branch testng commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,74 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void AllRemainTest() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void OneRemainTest() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void HalfRemainTest() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+
+        int actual = service.remain(amount);
+        int expected = 500;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void NineNineNineRemainTest() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ZeroRemainTest() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void MoreRemainTest() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+}
